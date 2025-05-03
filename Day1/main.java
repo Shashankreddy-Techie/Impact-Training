a12b5b0a48540fa1763f0162ec882e36d00dc6dc
NUKALA SHASHANK REDDY <152247010+Shashankreddy-Techie@users.noreply.github.com>
2025-05-03 12:00:41 +0530

    Add files via upload

---
+++ Day1/main.java
@@ -0,0 +1,5 @@
+class Shashank{
+    public static void main(String[] args) {
+        System.out.println("Hello, Shashank!");
+    }
+}
---
+++ Day1/operations.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class operations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter two numbers:");
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // System.out.println("000"+a);
+        // System.out.println("000"+b);
+        System.out.print("Enter a number:");
+        int a = sc.nextInt();
+        System.out.println(3-a);
+    }
+}
